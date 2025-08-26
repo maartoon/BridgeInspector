3480d3e92f40af15d397f0eb25de5ac1d85dfbac
maartoon <martinxu1234@gmail.com>
2025-08-26 12:19:44 -0500

    added drone info listener

---
+++ app/src/main/java/com/dji/bridgeinspector/LocationManager.kt
@@ -0,0 +1,73 @@
+import dji.sdk.keyvalue.key.FlightControllerKey
+import dji.sdk.keyvalue.key.CameraKey
+import dji.sdk.keyvalue.key.DJIKey
+import dji.sdk.keyvalue.value.camera.CameraOpticalZoomSpec
+import dji.v5.common.callback.CommonCallbacks
+import dji.v5.common.error.IDJIError
+import dji.v5.manager.KeyManager
+import dji.v5.utils.common.LogUtils
+
+
+class DroneDataManager {
+
+    private val TAG = "DroneDataManager
+
+    // listen for drone updates
+    fun startListening() {
+        // gps information - lat, long, alt
+        val locationKey = DJIKey.create(FlightControllerKey.KeyAircraftLocation3D)
+
+        // drone information - pitch, roll, yaw
+        val attitudeKey = DJIKey.create(FlightControllerKey.KeyAircraftAttitude)
+
+        // listen for location updates (provides old and new value, but we only care about new value)
+        KeyManager.getInstance().listen(locationKey, this) { _, location ->
+            location?.let {
+                val latitude = it.latitude
+                val longitude = it.longitude
+                val altitude = it.altitude
+
+                // log these values for testing, would want to process these values in the future
+                LogUtils.i(TAG, "Drone GPS: Lat: $latitude, Lon: $longitude, Alt: $altitude")
+            }
+        }
+
+        // listen for attitude updates
+        KeyManager.getInstance().listen(attitudeKey, this) { _, attitude ->
+            attitude?.let {
+                val pitch = it.pitch
+                val roll = it.roll
+                val yaw = it.yaw
+
+                // log values for now
+                LogUtils.i(TAG, "Drone Attitude: Yaw: $yaw, Pitch: $pitch, Roll: $roll")
+            }
+        }
+    }
+
+    // function to stop listening
+    fun stopListening() {
+        KeyManager.getInstance().cancelListen(this)
+    }
+
+    // fetch camera intrinsics
+    fun getCameraIntrinsics() {
+        val cameraIntrinsicsKey = DJIKey.create(CameraKey.KeyCameraOpticalZoomSpec)
+
+        KeyManager.getInstance().getValue(cameraIntrinsicsKey, object : CommonCallbacks.CompletionCallbackWithParam<CameraOpticalZoomSpec> {
+            override fun onSuccess(spec: CameraOpticalZoomSpec?) {
+                // spec may be null
+                spec?.let {
+                    // todo: derive fx, fy from focal length ranges; cx, cy
+                    val focalLengthMin = it.minFocalLength
+                    val focalLengthMax = it.maxFocalLength
+                    LogUtils.i(TAG, "Camera Focal Length Range: $focalLengthMin - $focalLengthMax")
+                }
+            }
+
+            override fun onFailure(error: IDJIError) {
+                LogUtils.e(TAG, "Failed to get camera intrinsics: ${error.description()}")
+            }
+        })
+    }
+}
